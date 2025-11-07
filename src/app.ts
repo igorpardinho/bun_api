d350efd10d1ca1048c6b90876574f475ee0df681
igorpardinho <pardinhoigor@gmail.com>
2025-11-07 13:15:31 -0300

    fix: remove os pacotes do eslint e renomeia o arquivo que roda o servidor da aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import swagger from "@elysiajs/swagger";
 import Elysia from "elysia";
+import { userRoutes } from "./routes/user.routes";
 
-export const app = new Elysia().get("/", () => "API Bun + Elysia está online");
+export const app = new Elysia()
+	.use(
+		swagger({
+			documentation: {
+				info: {
+					title: "Bun Api",
+					description: "Api Rest construida com elysia e drizzleOrm",
+					version: "1.0.0",
+				},
+			},
+			path: "/docs",
+		}),
+	)
+	.use(userRoutes);
